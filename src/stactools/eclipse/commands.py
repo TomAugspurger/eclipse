6e272484ddb960158abc78657481f5b620ccc178
Tom Augspurger <taugspurger@microsoft.com>
2022-04-07 16:22:31 -0500

    example [ci skip]

--- src/stactools/eclipse/commands.py
+++ src/stactools/eclipse/commands.py
@@ -52,18 +52,31 @@ def create_eclipse_command(cli):
         return None
 
     @eclipse.command("create-item", short_help="Create a STAC item")
-    @click.argument("source")
     @click.argument("destination")
-    def create_item_command(source: str, destination: str):
+    def create_item_command(destination: str):
         """Creates a STAC Item
 
         Args:
             source (str): HREF of the Asset associated with the Item
             destination (str): An HREF for the STAC Collection
         """
-        item = stac.create_item(source)
+        import planetary_computer
 
-        item.save_object(dest_href=destination)
+        sas_token = planetary_computer.sas.get_token(
+            stac.ACCOUNT_NAME, stac.CONTAINER_NAME
+        ).token
+        storage_options = {"account_name": stac.ACCOUNT_NAME, "credential": sas_token}
+        asset_extra_fields = {
+            "table:storage_options": {"account_name": stac.ACCOUNT_NAME}
+        }
+        sample_item = stac.create_item(
+            "eclipse/Chicago/2021-10-10",
+            protocol="abfs",
+            storage_options=storage_options,
+            asset_extra_fields=asset_extra_fields,
+        )
+
+        sample_item.save_object(dest_href=destination)
 
         return None
 
